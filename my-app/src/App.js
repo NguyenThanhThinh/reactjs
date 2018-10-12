93c11ce9e846220659b2e1a49625220119c9ab70
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-10-12 14:01:52 +0700

    add layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,21 +5,46 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <header>✉ Contact Book</header>
+        <div id="book">
+          <div id="list">
+            <h1>Contacts</h1>
+            <div className="content">
+              <div className="contact" data-id="id">
+                <span className="avatar small">☻</span>
+                <span className="title">Thinh Nguyen</span>
+              </div>
+              <div className="contact" data-id="id">
+                <span className="avatar small">☻</span>
+                <span className="title">Thanh Thinh</span>
+              </div>
+              <div className="contact" data-id="id">
+                <span className="avatar small">☻</span>
+                <span className="title">Messi</span>
+              </div>
+            </div>
+          </div>
+          <div id="details">
+            <h1>Details</h1>
+            <div className="content">
+              <div className="info">
+                <div className="col">
+                  <span className="avatar">☻</span>
+                </div>
+                <div className="col">
+                  <span className="name">Thanh</span>
+                  <span className="name">Thinh</span>
+                </div>
+              </div>
+              <div className="info">
+                <span className="info-line">☎ 0887 123 456</span>
+                <span className="info-line">✉ thanhthinhcntt@gmail.com</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer>Contact Book SPA © 2018</footer>
       </div>
     );
   }
